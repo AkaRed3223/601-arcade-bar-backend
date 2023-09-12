bf0b3aec0a160acc30d291bce7c2acd908e1ff51
AkaRed3223 <cesarsales22@gmail.com>
2023-09-12 00:54:17 -0300

    add docker compose and aws database

---
+++ src/main/java/com/arcade/MyRunner.java
@@ -0,0 +1,40 @@
+/*
+package com.arcade;
+
+import com.arcade.model.request.CategoryRequest;
+import com.arcade.model.request.ProductRequest;
+import com.arcade.model.request.TabRequest;
+import com.arcade.service.CategoriesService;
+import com.arcade.service.ProductsService;
+import com.arcade.service.TabsService;
+import lombok.AllArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@AllArgsConstructor
+@Component
+public class MyRunner implements CommandLineRunner {
+
+    private CategoriesService categoriesService;
+    private ProductsService productsService;
+    private TabsService tabsService;
+
+    @Override
+    public void run(String... args) throws Exception {
+        categoriesService.insert(new CategoryRequest("Lanches", null));
+        categoriesService.insert(new CategoryRequest("Bebidas", null));
+        categoriesService.insert(new CategoryRequest("Porções", null));
+
+        productsService.insert(new ProductRequest("X-Burger", "24.90", 1L));
+        productsService.insert(new ProductRequest("X-Bacon", "26.90", 1L));
+        productsService.insert(new ProductRequest("X-Pagan", "26.90", 1L));
+        productsService.insert(new ProductRequest("Cerveja Original", "12.90", 2L));
+        productsService.insert(new ProductRequest("Cerveja Heineken", "14.90", 2L));
+        productsService.insert(new ProductRequest("Coca-Cola", "6.90", 2L));
+        productsService.insert(new ProductRequest("Batata frita", "14.90", 3L));
+
+        tabsService.insert(new TabRequest("Cesar", 64L));
+        tabsService.insert(new TabRequest("Leo", 65L));
+    }
+}
+*/
